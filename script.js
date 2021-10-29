0c9fee39b6d8f115ff31c220912a534d8969615d
jpbetanza <jpbetanza99@gmail.com>
2021-10-29 07:54:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+function printMousePos(event) {
+      console.log("clientX: " + event.clientX + " - clientY: " + event.clientY);
+  }
+  
+document.addEventListener("click", printMousePos);
+
+var digitos = [
+    ['A', 'B', 'C','','','', 'D', 'E', 'F', ],
+    ['G', 'H', 'I','','','', 'J', 'K', 'L', ],
+    ['M', 'N', 'O','','','', 'P', 'Q', 'R', ],
+    ['', '', '','','','', '', '', '', ],
+    ['', '', '','','','', '', '', '', ],
+    ['', '', '','','','', '', '', '', ],
+    ['S', 'T', 'U','','','', 'V', 'W', 'X', ],
+    ['Y', 'Z', '1','','','', '2', '3', '4', ],
+    ['5', '6', '7','','','', '8', '9', '0', ]
+]
+
+function estadoInicial() {
+    for (i = 1; i < 10; i++) {
+        if (i == 5) {} else {
+            document.getElementById('' + i + '').src = './src/Quadros/quadro' + i + '.png'
+            document.getElementById('' + i + '').setAttribute('onclick', 'selecaoDeQuadro(' + i + ')')
+        }
+    }
+}
+
+function selecaoDeQuadro(a) {
+    switch (a) {
+        case 1:
+            for (i = 1; i < 10; i++) {
+                if (i == 4 || i == 6) {
+                    document.getElementById('' + i + '').src = './src/white.png'
+                } else if (i == 5) {} else {
+                    document.getElementById('' + i + '').src = './src/LetrasAF/' + i + '.png'
+                    document.getElementById('' + i + '').setAttribute('onclick', 'selecaoDeLetra(' + 1 + ',' + i + ')')
+                }
+            }
+            break;
+        case 2:
+            for (i = 1; i < 10; i++) {
+                if (i == 4 || i == 6) {
+                    document.getElementById('' + i + '').src = './src/white.png'
+                } else if (i == 5) {} else {
+                    document.getElementById('' + i + '').src = './src/LetrasGL/' + i + '.png'
+                    document.getElementById('' + i + '').setAttribute('onclick', 'selecaoDeLetra(' + 2 + ',' + i + ')')
+                }
+            }
+            break;
+        case 3:
+            for (i = 1; i < 10; i++) {
+                if (i == 4 || i == 6) {
+                    document.getElementById('' + i + '').src = './src/white.png'
+                } else if (i == 5) {} else {
+                    document.getElementById('' + i + '').src = './src/LetrasMR/' + i + '.png'
+                    document.getElementById('' + i + '').setAttribute('onclick', 'selecaoDeLetra(' + 3 + ',' + i + ')')
+                }
+            }
+            break;
+            /*case 4:
+                for (i = 1; i < 10; i++) {
+                    if (i == 4 || i == 6) {
+                        break;
+                    } else {
+                        document.getElementById('' + i + '').src = './src/LetrasSX/' + i + '.png'
+                        document.getElementById('' + i + '').setAttribute('onclick',) = 'selecaoDeLetra(' + 4 + ',' + i + ')'
+                    }
+                }
+                break;
+            */
+            /*case 6:
+                
+                break;
+            */
+        case 7:
+            for (i = 1; i < 10; i++) {
+                if (i == 4 || i == 6) {
+                    document.getElementById('' + i + '').src = './src/white.png'
+                } else if (i == 5) {} else {
+                    document.getElementById('' + i + '').src = './src/LetrasSX/' + i + '.png'
+                    document.getElementById('' + i + '').setAttribute('onclick', 'selecaoDeLetra(' + 7 + ',' + i + ')')
+                }
+            }
+            break;
+        case 8:
+            for (i = 1; i < 10; i++) {
+                if (i == 4 || i == 6) {
+                    document.getElementById('' + i + '').src = './src/white.png'
+                } else if (i == 5) {} else {
+                    document.getElementById('' + i + '').src = './src/LetrasY4/' + i + '.png'
+                    document.getElementById('' + i + '').setAttribute('onclick', 'selecaoDeLetra(' + 8 + ',' + i + ')')
+                }
+            }
+            break;
+        case 9:
+            for (i = 1; i < 10; i++) {
+                if (i == 4 || i == 6) {
+                    document.getElementById('' + i + '').src = './src/white.png'
+                } else if (i == 5) {} else {
+                    document.getElementById('' + i + '').src = './src/Letras50/' + i + '.png'
+                    document.getElementById('' + i + '').setAttribute('onclick', 'selecaoDeLetra(' + 9 + ',' + i + ')')
+                }
+            }
+            break;
+
+    }
+}
+
+function selecaoDeLetra(a, b) {
+    texto = document.getElementById('textbox').value
+    document.getElementById('textbox').value = texto+digitos[a-1][b-1]
+    estadoInicial()
+}
---
+++ trackerManager.js
@@ -0,0 +1,75 @@
+window.saveDataAcrossSessions = false //salva a calibração mesmo se fechar o navegador
+//variaveis de tempo necessario para olhar um quadro e tempo olhado
+const DELAY = 1500; //tempo olhando para algo para clicar
+let tempoOlhando = Number.POSITIVE_INFINITY //variavel do tempo (cronometro)
+let olhandoAlgo = false
+let rect
+let quadro
+
+// Função para clicar em um ponto X,Y da tela
+function simulateClick(x, y) {
+    var clickEvent= document.createEvent('MouseEvents');
+    clickEvent.initMouseEvent(
+    'click', true, true, window, 0,
+    0, 0, x, y, false, false,
+    false, false, 0, null
+    );
+    document.elementFromPoint(x, y).dispatchEvent(clickEvent);
+}
+
+//Funcionamento do webgazer.js
+webgazer.setGazeListener(function(data, elapsedTime) {
+    
+    if (data == null) { return; } // Se não houver dados, não faça nada
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+     // Identificação do quadro observado
+    
+    //trabalhando o tempo olhado
+    if(quadro != null && olhandoAlgo == false){
+        console.log('olhei pra dentro')
+        console.log(document.elementsFromPoint(xprediction, yprediction))
+        tempoOlhando = elapsedTime //Se um quadro estiver sendo olhado, "inicia-se a contagem"
+        olhandoAlgo = true
+        rect = quadro.getBoundingClientRect();
+    }
+    if(!document.elementsFromPoint(xprediction, yprediction).includes(quadro) && olhandoAlgo == true){
+        console.log('olhei pra fora')
+        console.log(quadro)
+        console.log(document.elementsFromPoint(xprediction, yprediction))
+
+        tempoOlhando = Number.POSITIVE_INFINITY //Se não, reseta-se a contagem
+        olhandoAlgo = false
+        rect = null
+    }
+    if ((tempoOlhando + DELAY < elapsedTime)){
+        console.log('passei 2 segundos olhando')
+        simulateClick((rect['x']+rect['right'])/2,(rect['y']+rect['bottom'])/2) // clica na posição X,Y da bolinha vermelha
+        tempoOlhando = Number.POSITIVE_INFINITY //Se não, reseta-se a contagem
+        olhandoAlgo = false
+        rect = null
+    }
+    console.log(rect)
+    pickup = document.elementsFromPoint(xprediction,yprediction)
+    console.log(pickup)
+
+    if (pickup==null || pickup.length < 2){
+        quadro = null
+    }
+    else if(pickup[0]['nodeName']=='IMG'){
+        quadro = pickup[0]
+    }
+    else if(pickup[1]['nodeName']=='IMG'){
+        quadro = pickup[1]
+    }
+    else if(pickup[0]['nodeName']=='TEXTAREA'){
+        quadro = pickup[0]
+    }
+    else if(pickup[1]['nodeName']=='TEXTAREA'){
+        quadro = pickup[1]
+    }
+
+
+}).begin();
+
+webgazer.showVideoPreview(false) //substitua por "true" se quiser ver a camera
