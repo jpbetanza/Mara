df7e391810ac6fc1177965756bc85736b325c3d7
jpbetanza <jpbetanza99@gmail.com>
2021-11-01 15:24:59 -0300

    Calibração pode ser acessada pela pagina do teclado e vice-versa. Calibração não gera mais pontos aleatorios e simulam a posição dos quadros do teclado.

--- Calibrador/scriptCalibrador.js
+++ Calibrador/scriptCalibrador.js
@@ -1,27 +1,47 @@
 function printMousePos(event) {
-      console.log("clientX: " + event.clientX + " - clientY: " + event.clientY);
-  }
-document.addEventListener("click", printMousePos);
-let counter = 0
+  console.log("clientX: " + event.clientX + " - clientY: " + event.clientY);
+}
 
+document.addEventListener("click", printMousePos);
+let larg = document.documentElement.scrollWidth
+let altu = document.documentElement.scrollHeight
+
+let counter = 0
+let positionCounter = 0
+let position = [
+  [larg/6-25, altu/6-25],
+  [larg/2-25, altu/6-25],
+  [larg*(5/6)-50, altu/6-25],
+  [larg*(5/6)-50, altu/2-25],
+  [larg*(5/6)-50, altu*(5/6)-50],
+  [larg/2-25, altu*(5/6)-50],
+  [larg/6-25, altu*(5/6)-50],
+  [larg/6-25, altu/2-25],
+  [larg/2-25, altu/2-25],
+]
 
 localStorage.setItem('PosY', 25)
 localStorage.setItem('PosX', 25)
 
 
 
-function cliquei(){
+function cliquei() {
   counter++;
-  if(counter==3){
-    counter=0;
+  if (counter == 3) {
+    counter = 0;
+    if(positionCounter<8){
+    positionCounter++;
+    }
+    else{
+      positionCounter=0
+    }
+    posx = position[positionCounter][0]
+    posy = position[positionCounter][1]
 
-    posy = (Math.random() * document.documentElement.scrollHeight*0.9)
-    posx = (Math.random() * document.documentElement.scrollWidth*0.9)
+    localStorage.setItem('PosX', posx + 25)
+    localStorage.setItem('PosY', posy + 25)
 
-    localStorage.setItem('PosX',posx+25)
-    localStorage.setItem('PosY',posy+25)
-    
-    document.getElementById("circle").style.left = posx+'px'
-    document.getElementById("circle").style.top = posy+'px'
+    document.getElementById("circle").style.left = posx + 'px'
+    document.getElementById("circle").style.top = posy + 'px'
   }
 }
